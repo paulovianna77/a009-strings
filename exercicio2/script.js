a386e74a9b85077699396bea79b5b672b052be28
paulovianna <paulo2001.vianna@gmail.com>
2022-07-26 16:53:29 -0300

    Exercícios 1, 2, 3 de a009-strings resolvidos.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const novaString = minhaString.trim()
+
+
+console.log(`Antes de tirar os espaços sobrandos temos ${minhaString.length} caracteres. \n
+Agora depois de tirar os espaços indevidos temos ${novaString.length} caracteres.\n
+Antes ela estava incompleta [${novaString}] \n
+Agora está completa [${minhaString.replaceAll("________","sticioso")}]`)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,7 @@
 //Crie a const para a frase aqui
+const frase =  "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"";
+const novaFrase = frase.replaceAll("verde","rosa")
+const novafrase2 = novaFrase.replaceAll("azul","laranja")
+const novafrase3 = novafrase2.replaceAll(" mas não deixe o gato sair"," MAS NÃO DEIXE O GATO SAIR")
+
+console.log(novafrase3)
